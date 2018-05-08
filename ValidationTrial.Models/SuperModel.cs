b7979ae51987232b314fdac846f518640971d30b
Josh Robinson <josh.robinson@gmail.com>
2018-05-08 19:07:45 -0400

    first commit with project structure

---
+++ ValidationTrial.Models/SuperModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidationTrial.Models
+{
+    //https://www.vogue.com/article/the-top-models-fall-2018-collections
+    public class SuperModel
+    {
+        public string ModelName { get; set; }
+
+        public string Hometown { get; set; }
+
+        public List<string> Designers { get; set; }
+    }
+}
